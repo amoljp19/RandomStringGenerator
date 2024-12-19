756721e564fdda52afe8e191aaf1644a1eae34fc
amoljp19 <41947289+amoljp19@users.noreply.github.com>
2024-12-20 01:10:48 +0530

    created provider with query and delete method

--- app/src/main/java/com/softaai/randomstringgenerator/provider/MyContentProvider.kt
+++ app/src/main/java/com/softaai/randomstringgenerator/provider/MyContentProvider.kt
@@ -2,7 +2,12 @@ package com.softaai.randomstringgenerator.provider
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Context
+import android.content.UriMatcher
 import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.database.sqlite.SQLiteQueryBuilder
 import android.net.Uri
 
 
@@ -12,42 +17,154 @@ import android.net.Uri
  */
 class MyContentProvider : ContentProvider() {
 
+
+
+
     companion object {
         // defining authority so that other application can access it
-        const val PROVIDER_NAME = "com.demo.user.provider"
+        const val AUTHORITY_NAME = "com.iav.contestdataprovider"
 
         // defining content URI
-        const val URL = "content://$PROVIDER_NAME/users"
-    }
+        const val URL = "content://$AUTHORITY_NAME/text"
 
+        // parsing the content URI
+        val CONTENT_URI = Uri.parse(URL)
+        const val id = "id"
+        const val COLUMN_NAME: String = "data"
+
+        const val uriCode = 1
+        var uriMatcher: UriMatcher? = null
+
+        private val values: HashMap<String, String>? = null
+
+
+        // declaring name of the database
+        const val DATABASE_NAME = "RandomStringDB"
+
+        // declaring table name of the database
+        const val TABLE_NAME = "RandomStrings"
+
+        // declaring version of the database
+        const val DATABASE_VERSION = 1
+
+        // sql query to create the table
+        const val CREATE_DB_TABLE =
+            (" CREATE TABLE " + TABLE_NAME
+                    + " (id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + " data TEXT NOT NULL);")
+
+        init {
+
+            // to match the content URI
+            // every time user access table under content provider
+            uriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+
+            // to access whole table
+            uriMatcher!!.addURI(
+                AUTHORITY_NAME,
+                "text",
+                uriCode
+            )
+
+            // to access a particular row
+            // of the table
+            uriMatcher!!.addURI(
+                AUTHORITY_NAME,
+                "text/*",
+                uriCode
+            )
+        }
+
+
+    }
+    // creating the database
     override fun onCreate(): Boolean {
-        TODO("Not yet implemented")
+        val context = context
+        val dbHelper =
+            DatabaseHelper(context)
+        db = dbHelper.writableDatabase
+        return if (db != null) {
+            true
+        } else false
     }
 
     override fun query(
-        p0: Uri,
-        p1: Array<out String>?,
-        p2: String?,
-        p3: Array<out String>?,
-        p4: String?
+        uri: Uri, projection: Array<String>?, selection: String?,
+        selectionArgs: Array<String>?, sortOrder: String?
     ): Cursor? {
-        TODO("Not yet implemented")
+        var sortOrder = sortOrder
+        val qb = SQLiteQueryBuilder()
+        qb.tables = TABLE_NAME
+        when (uriMatcher!!.match(uri)) {
+            uriCode -> qb.projectionMap = values
+            else -> throw IllegalArgumentException("Unknown URI $uri")
+        }
+        if (sortOrder == null || sortOrder === "") {
+            sortOrder = id
+        }
+        val c = qb.query(
+            db, projection, selection, selectionArgs, null,
+            null, sortOrder
+        )
+        c.setNotificationUri(context!!.contentResolver, uri)
+        return c
     }
 
-    override fun getType(p0: Uri): String? {
-        TODO("Not yet implemented")
+    override fun getType(uri: Uri): String? {
+        return when (uriMatcher!!.match(uri)) {
+            uriCode -> "vnd.android.cursor.dir/text"
+            else -> throw IllegalArgumentException("Unsupported URI: $uri")
+        }
     }
 
     override fun insert(p0: Uri, p1: ContentValues?): Uri? {
         TODO("Not yet implemented")
     }
 
-    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
-        TODO("Not yet implemented")
+    override fun delete(uri: Uri,
+                        selection: String?,
+                        selectionArgs: Array<String>?): Int {
+        var count = 0
+        count = when (uriMatcher!!.match(uri)) {
+            uriCode -> db!!.delete(TABLE_NAME, selection, selectionArgs)
+            else -> throw IllegalArgumentException("Unknown URI $uri")
+        }
+        context!!.contentResolver.notifyChange(uri, null)
+        return count
     }
 
     override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
         TODO("Not yet implemented")
+    }
+
+    // creating object of database
+    // to perform query
+    private var db: SQLiteDatabase? = null
+
+    // creating a database
+    private class DatabaseHelper  // defining a constructor
+    internal constructor(context: Context?) : SQLiteOpenHelper(
+        context,
+        DATABASE_NAME,
+        null,
+        DATABASE_VERSION
+    ) {
+        // creating a table in the database
+        override fun onCreate(db: SQLiteDatabase) {
+            db.execSQL(CREATE_DB_TABLE)
+        }
+
+        override fun onUpgrade(
+            db: SQLiteDatabase,
+            oldVersion: Int,
+            newVersion: Int
+        ) {
+
+            // sql query to drop a table
+            // having similar name
+            db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+            onCreate(db)
+        }
     }
 
 }
